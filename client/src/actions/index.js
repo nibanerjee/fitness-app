044692310cfc4baff38f66e507645fc1f1f7871b
Banerjee, Nilanjan (US - Bengaluru) <nibanerjee@deloitte.com>
2019-07-17 16:36:40 +0530

    saving user data on bmi calc

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,5 @@
+import fitnessService from '../services/FitnessService';
+
 export const signIn = (userId) => {
     return {
         type : 'SIGN_IN',
@@ -9,4 +11,28 @@ export const signOut = () => {
     return {
         type : 'SIGN_OUT'
     } 
+}
+
+export const PersistBMIData = (bmi) => {
+   return async (dispatch,getState) => {
+        const {userId} = getState().auth;
+        const response = await fitnessService.post('/users',{bmi,userId});
+        dispatch({
+            type : 'PERSIST_BMI',
+            payload : response.data
+        });
+   }
+}
+
+export const FetchBMIData = () => {
+    return async (dispatch,getState) => {
+        const {userId} = getState().auth;
+        const response = await fitnessService.get(`/users?userId=${userId}`);
+        if(response.data.length){
+            dispatch({
+                type : 'FETCH_BMI',
+                payload : response.data
+            });
+        }
+    }
 }
