aa1d6453c44eff5b043d9d79884778b339434f85
Banerjee, Nilanjan (US - Bengaluru) <nibanerjee@deloitte.com>
2019-07-20 13:51:07 +0530

    checking in the build folder

---
+++ client/build/precache-manifest.9e6f9a7048ced8b8fffaf7c98c6fc2ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3f5a7be2975094936be3818113d2d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49bccb2c4ef45e4f012a",
+    "url": "/static/css/main.2583558b.chunk.css"
+  },
+  {
+    "revision": "f987860f04b2eda091a7",
+    "url": "/static/js/2.87162ff9.chunk.js"
+  },
+  {
+    "revision": "49bccb2c4ef45e4f012a",
+    "url": "/static/js/main.2fd22195.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "97abefe600996f82773ade5dbf9377d5",
+    "url": "/static/media/home-bg.97abefe6.jpg"
+  }
+]);
